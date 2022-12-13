77c1ebbf39fd668df959539f6ec31f47d026a939
Hiyabo <99443606+hiyabo69@users.noreply.github.com>
2022-12-12 23:21:10 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+import ProxyCloud
+# Bot
+BOT_TOKEN = '5506081827:AAGPAXNcMsB1ZD8fp5Sc6GQB90X2y7xNRlM'
+TG_API_ID = 13233271
+TG_API_HASH = 'e3ce8145aa657c2a4cc5cf0f7183e476'
+TG_ADMIN = 'Hiyabo'
+TG_USER = 'EL_Wizard'
+TG_USER1 = 'Stvz02'
+TG_USER2 = 'valdes_95'  
+TG_USER3 = 'gyi'
+
+
+# Database
+DB_LOCAL = False
+DB_HOST = 'sql.freedb.tech'
+DB_HOST_USERNAME = 'freedb_hiyabo'
+DB_HOST_PASSWORD = '?vj&ZrZhGu5txSW'
+DB_NAME = 'freedb_educaDb'
+
+if DB_LOCAL:
+    # Database Local
+    DB_HOST = ''
+    DB_HOST_USERNAME = ''
+    DB_HOST_PASSWORD = ''
+    DB_NAME = 'clutilprodb'
